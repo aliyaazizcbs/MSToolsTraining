44d4178a9ef83c6ff7865d7345586bdf2359ae3a
CBS\aaziz <aaziz@carbenefitsolutions.co.uk>
2026-05-08 09:41:12 +0100

    Solution can be seen at /solution.

--- MSToolsDemo/frontend/src/App.tsx
+++ MSToolsDemo/frontend/src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SignupForm from './SignupForm'
+import SignupFormSolution from './SignupFormSolution'
 
 function App() {
-  return <SignupForm />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignupForm />} />
+        <Route path="/solution" element={<SignupFormSolution />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
